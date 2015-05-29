6620b9387c140b61d146a9fceb7a10eb9687449f
Charlie Cooper <charlie@fancygiving.com>
2015-05-29 21:55:05 +0100

    Spring profile for testing

--- src/test/java/com/pluralsight/orderfulfillment/order/NewWebsiteOrderRouteTest.java
+++ src/test/java/com/pluralsight/orderfulfillment/order/NewWebsiteOrderRouteTest.java
@@ -9,6 +9,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
@@ -25,6 +26,7 @@ import com.pluralsight.orderfulfillment.test.TestIntegration;
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { TestIntegration.class, IntegrationConfig.class })
+@ActiveProfiles("test")
 public class NewWebsiteOrderRouteTest {
 
 	@Inject
--- src/test/java/com/pluralsight/orderfulfillment/test/TestIntegration.java
+++ src/test/java/com/pluralsight/orderfulfillment/test/TestIntegration.java
@@ -5,9 +5,11 @@ import javax.sql.DataSource;
 import org.apache.commons.dbcp.BasicDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
+@Profile("test")
 public class TestIntegration {
 	
 	/**
