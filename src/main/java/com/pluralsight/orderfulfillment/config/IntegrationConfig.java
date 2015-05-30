3ade417c7fec8f7b2dcc72fc67cbe911a4a96a86
Charlie Cooper <charlie@fancygiving.com>
2015-05-30 14:24:51 +0100

    update route to send to JMS queue

--- src/main/java/com/pluralsight/orderfulfillment/config/IntegrationConfig.java
+++ src/main/java/com/pluralsight/orderfulfillment/config/IntegrationConfig.java
@@ -1,12 +1,18 @@
 package com.pluralsight.orderfulfillment.config;
 
 import javax.inject.Inject;
+import javax.jms.ConnectionFactory;
 
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.activemq.camel.component.ActiveMQComponent;
+import org.apache.activemq.jms.pool.PooledConnectionFactory;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.component.jms.JmsConfiguration;
 import org.apache.camel.component.sql.SqlComponent;
 import org.apache.camel.spring.javaconfig.CamelConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
 
 import com.pluralsight.orderfulfillment.order.OrderStatus;
 
@@ -23,7 +29,56 @@ public class IntegrationConfig extends CamelConfiguration {
 
 	   @Inject
 	   private javax.sql.DataSource dataSource;
+	   
+	   @Inject
+	   private Environment environment;
 
+	   /**
+	    * ActiveMQConnectionFactory bean for creating connections
+	    * 
+	    * @return
+	    */
+	   @Bean
+	   public ConnectionFactory jmsConnectionFactory() {
+		   return new ActiveMQConnectionFactory(environment.getProperty("activemq.broker.url"));
+	   }
+	   
+	   /**
+	    * PooledConnectionFactory
+	    * 
+	    * @return
+	    */
+	   @Bean(initMethod = "start", destroyMethod = "stop")
+	   public PooledConnectionFactory pooledConnectionFactory() {
+		   PooledConnectionFactory factory = new PooledConnectionFactory();
+		   factory.setConnectionFactory(jmsConnectionFactory());
+		   factory.setMaxConnections(Integer.parseInt(environment.getProperty("pooledConnectionFactory.maxConnections")));
+		   return factory;
+	   }
+	   
+	   /**
+	    * JMSConfiguration
+	    * 
+	    * @return
+	    */
+	   @Bean
+	   public JmsConfiguration jmsConfiguration() {
+		   JmsConfiguration jmsConfiguration = new JmsConfiguration(pooledConnectionFactory());
+		   return jmsConfiguration;
+	   }
+	   
+	   /**
+	    * ActiveMQComponent
+	    * 
+	    * @return
+	    */
+	   @Bean
+	   public ActiveMQComponent activeMQ() {
+		   ActiveMQComponent activeMQ = new ActiveMQComponent();
+		   activeMQ.setConfiguration(jmsConfiguration());
+		   return activeMQ;
+	   }
+	   
 	   /**
 	    * SQL Component instance used for routing orders from the orders database
 	    * and updating the orders database.
@@ -42,6 +97,8 @@ public class IntegrationConfig extends CamelConfiguration {
 	    * orders with status set to new, then updates the order status to be in
 	    * process. The route sends the message exchange to a log component.
 	    * 
+	    * Added a message translator bean to translate the bean to XML using JAXB.
+	    * 
 	    * @return
 	    */
 	   @Bean
@@ -58,7 +115,8 @@ public class IntegrationConfig extends CamelConfiguration {
 	                        + "' where id = :#id").
 	                        // route exchange to this bean`s method
 	                        beanRef("orderItemMessageTranslator", "transformToOrderItemMessage").
-                to("log:com.pluralsight.orderfulfillment.order?level=INFO");
+	                        // to("log:com.pluralsight.orderfulfillment.order?level=INFO");
+	                        to("activemq:queue:ORDER_ITEM_PROCESSING");
 	         }
 	      };
 	   }
