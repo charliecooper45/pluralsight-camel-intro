90a1480fb049fed75ab44b5fb75fd85bbc8333c6
Charlie Cooper <charlie@fancygiving.com>
2015-05-31 07:38:40 +0100

    content based router with xpath

--- src/main/java/com/pluralsight/orderfulfillment/config/IntegrationConfig.java
+++ src/main/java/com/pluralsight/orderfulfillment/config/IntegrationConfig.java
@@ -7,6 +7,7 @@ import org.apache.activemq.ActiveMQConnectionFactory;
 import org.apache.activemq.camel.component.ActiveMQComponent;
 import org.apache.activemq.jms.pool.PooledConnectionFactory;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.builder.xml.Namespaces;
 import org.apache.camel.component.jms.JmsConfiguration;
 import org.apache.camel.component.sql.SqlComponent;
 import org.apache.camel.spring.javaconfig.CamelConfiguration;
@@ -116,8 +117,55 @@ public class IntegrationConfig extends CamelConfiguration {
 	                        // route exchange to this bean`s method
 	                        beanRef("orderItemMessageTranslator", "transformToOrderItemMessage").
 	                        // to("log:com.pluralsight.orderfulfillment.order?level=INFO");
-	                        to("activemq:queue:ORDER_ITEM_PROCESSING");
+                to("activemq:queue:ORDER_ITEM_PROCESSING");
 	         }
+	      };
+	   }
+	   
+	   /**
+	    * Route builder to implement a Content-Based Router. Routes the message from
+	    * the ORDER_ITEM_PROCESSING queue to the appropriate queue based on the
+	    * fulfillment center element of the message. As the message from the
+	    * ORDER_ITEM_PROCESSING queue is XML, a namespace is required. A Choice
+	    * processor is used to realize the Content-Based Router. When the
+	    * Fulfillment Center element is equal to the value of the ABC fulfillment
+	    * center enumeration, the message will be routed to the ABC fulfillment
+	    * center request queue. When the Fulfillment Center element is equal to the
+	    * value of the Fulfillment Center 1 enumeration value, the message will be
+	    * routed to the Fulfillment Center 1 request queue. If a message comes in
+	    * with a Fulfillment Center element value that is unsupported, the message
+	    * gets routed to an error queue. An XPath expression is used to lookup the
+	    * fulfillment center value using the specified namespace.
+	    * 
+	    * Below is a snippet of the XML returned by the ORDER_ITEM_PROCESSING queue.
+	    * 
+	    * <Order xmlns="http://www.pluralsight.com/orderfulfillment/Order">
+	    * <OrderType> <FulfillmentCenter>ABCFulfillmentCenter</FulfillmentCenter>
+	    * 
+	    * @return
+	    */
+	   @Bean
+	   public RouteBuilder fulfillmentCenterContentBasedRouter() {
+	      return new RouteBuilder() {
+	         @Override
+	         public void configure() throws Exception {
+	        	 // this is where we look up the element
+	        	 Namespaces namespace = new Namespaces("o", "http://www.pluralsight.com/orderfulfillment/Order");
+				
+	        	 // Send from the ORDER_ITEM_PROCESSING queue to the correct fulfillment center queue.
+	        	 from("activemq:queue:ORDER_ITEM_PROCESSING")
+				  	.choice()
+				  	.when()
+				  		.xpath("/o:Order/o:OrderType/o:FulfillmentCenter = '" + com.pluralsight.orderfulfillment.generated.FulfillmentCenter.ABC_FULFILLMENT_CENTER.value()
+			        		+ "'", namespace)
+		        		.to("activemq:queue:ABC_FULFILLMENT_REQUEST")
+	        		.when()
+	        			.xpath("/o:Order/o:OrderType/o:FulfillmentCenter = '" + com.pluralsight.orderfulfillment.generated.FulfillmentCenter.FULFILLMENT_CENTER_ONE.value()
+        					+ "'", namespace)
+    					.to("activemq:queue:FC1_FULFILLMENT_REQUEST")
+    				.otherwise()
+				  		.to("activemq:queue:ERROR_FULFILLMENT_REQUEST");
+				 }
 	      };
 	   }
 }
